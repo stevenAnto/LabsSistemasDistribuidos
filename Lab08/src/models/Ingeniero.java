bfe9d11b99d80c9bb3f2540dd6d8b7e5e1089d20
fvilcaqui <fvilcaqui@unsa.edu.pe>
2024-06-16 17:12:02 -0500

    Lab08 ejercicio2

---
+++ Lab08/src/models/Ingeniero.java
@@ -0,0 +1,37 @@
+package models;
+
+public class Ingeniero {
+    private int id;
+    private String nombre;
+    private String especialidad;
+
+    public Ingeniero(int id, String nombre, String especialidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+}

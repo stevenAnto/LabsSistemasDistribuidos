d8aee66e8f3e3519eae1b5f96fd61f51f7ca6e81
stevenAnto <ecalcinap@unsa.edu.pe>
2024-06-16 20:02:43 -0500

    query Ingenieros por Proyecto

---
+++ src/sql/Conexionsql.java
@@ -0,0 +1,35 @@
+package sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
+public class Conexionsql {
+    Connection conn = null;
+    String url = "jdbc:postgresql://localhost/proyecto_empresa";
+    String usuario = "postgres";
+    String clave = "12345";
+    
+    public void conectar() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(url, usuario, clave);
+            JOptionPane.showMessageDialog(null, "Conexion Exitosa", "conexion", JOptionPane.INFORMATION_MESSAGE);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Conexion Fallida" + e, "conexion", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+    public void desconectar() {
+        try {
+            conn.close();
+            JOptionPane.showMessageDialog(null, "Desconexion Exitosa", "deconexion", JOptionPane.INFORMATION_MESSAGE);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Desconexion Fallida" + e, "deconexion", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+
+}
---
+++ src/vistas/arranque.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vistas;
+
+
+public class arranque {
+
+    public static void main(String[] args) {
+        //DepartamentoFrame abrir= new DepartamentoFrame();
+        //IngenierosFrame abrir= new IngenierosFrame();
+        ProyectoFrame abrir = new ProyectoFrame();
+        //ConsultaCruzada abrir = new ConsultaCruzada();
+        abrir.setVisible(true);
+        abrir.setLocationRelativeTo(null);
+    }
+    
+}
